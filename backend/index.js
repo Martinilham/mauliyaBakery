40b54c5311e1ec72549c20d3b8dc02b21a851c88
Martin <ilhammartin2@gmail.com>
2024-02-21 23:19:00 +0700

    perubahan

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ import { ServerApiVersion } from 'mongodb'
 import barangRoutes from './routes/barangRoutes.js'
 import cors from "cors"
 
-const url = "mongodb://martinilham15:martinus12345@ac-p1s5fuc-shard-00-00.aw1tteh.mongodb.net:27017,ac-p1s5fuc-shard-00-01.aw1tteh.mongodb.net:27017,ac-p1s5fuc-shard-00-02.aw1tteh.mongodb.net:27017/mauliya_bakery?ssl=true&replicaSet=atlas-s1025i-shard-0&authSource=admin&retryWrites=true&w=majority";
+const url = "mongodb://martinilham155:wer1234ui@ac-p1s5fuc-shard-00-00.aw1tteh.mongodb.net:27017,ac-p1s5fuc-shard-00-01.aw1tteh.mongodb.net:27017,ac-p1s5fuc-shard-00-02.aw1tteh.mongodb.net:27017/mauliya_bakery?ssl=true&replicaSet=atlas-s1025i-shard-0&authSource=admin&retryWrites=true&w=majority";
 const port = 5000
 
 const app = express()
