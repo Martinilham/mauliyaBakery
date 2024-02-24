d7fa4b1bbb01736af94f2b29e91c0841b854021b
Martin <ilhammartin2@gmail.com>
2024-02-24 23:57:37 +0700

    p

---
+++ frontend/src/admin/pages/dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from "./layout.jsx";
+
+function Dashboard() {
+  return (
+    <div>
+      <Layout>
+        sanagar
+      </Layout>
+    </div>
+  )
+}
+export default Dashboard
---
+++ frontend/src/admin/pages/login.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div className='text-black'><p>iki login asu</p></div>
+  )
+}
+
+export default Login
