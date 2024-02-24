260275f9976428564881b256a937577dd4ee3b69
Martin <ilhammartin2@gmail.com>
2024-02-24 22:46:32 +0700

    set up baru

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,132 +1,24 @@
-import * as React from "react";
-import { BrowserRouter as Router, Route, Switch,Routes } from 'react-router-dom';
-import Box from "@mui/material/Box";
-import Sidebar from "./components/common/Sidebar";
-import Dashboard from "./pages/Dashboard";
-import Navbar from "./components/common/Navbar";
-import {
-  AddProduct,
-  Brands,
-  Customers,
-  Inbox,
-  Orders,
-  OrderTemplate,
-  ProductCategories,
-  Products,
-  ProductSales,
-  Reviews,
-  SalesAnalytics,
-  Settings,
-  SingleCustomer,
-  SingleOrder,
-  SingleProduct,
-  Suppliers,
-  Transactions,
-} from "./pages";
-import Footer from "./components/common/Footer";
-
-// const router = createBrowserRouter([
-//   {
-//     path:"/dashboard/",
-//     element:<Dashboard/>
-//   },
-//   {
-//     path:"/dashboard/products",
-//     element:<Products />
-//   },
-//   {
-//     path:"/dashboard/products/add",
-//     element:<AddProduct />
-//   },
-//   {
-//     path:"/dashboard/products/:id",
-//     element:<SingleProduct />
-//   },
-//   {
-//     path:"/dashboard/products/categories",
-//     element:<ProductCategories />
-//   },
-//   {
-//     path:"/dashboard/customers",
-//     element:<Customers />
-//   },
-//   {
-//     path:"/dashboard/customers/:id",
-//     element:<SingleCustomer />
-//   },
-//   {
-//     path:"/dashboard/sales/analysis",
-//     element:<SalesAnalytics />
-//   },
-//   {
-//     path:"/dashboard/sales",
-//     element:<ProductSales />
-//   },
-//   {
-//     path:"/dashboard/orders",
-//     element:<Orders />
-//   }
-// ])
-
-const sideBarWidth = 250;
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [mobileOpen, setMobileOpen] = React.useState(false);
-
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
-
   return (
-    <Box sx={{ display: "flex" }}>
-      <Navbar
-        sideBarWidth={sideBarWidth}
-        handleDrawerToggle={handleDrawerToggle}
-      />
-      <Sidebar
-        sideBarWidth={sideBarWidth}
-        mobileOpen={mobileOpen}
-        handleDrawerToggle={handleDrawerToggle}
-      />
-      <Box
-        component="main"
-        sx={{
-          flexGrow: 1,
-          px: { xs: 1, md: 2 },
-          width: { xs: "100%", md: `calc(100% - ${sideBarWidth}px)` },
-        }}
-      >
-        {/* <React.StrictMode>
-          <RouterProvider router={router} />
-        </React.StrictMode> */}
-        {/* Routes */}
-        <Routes>
-          
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/dashboard/products/add" component={<AddProduct />} />
-          <Route path="/dashboard/products/:id" component={<SingleProduct />} />
-          <Route path="/dashboard/products/categories" component={<ProductCategories />} />
-          <Route path="/dashboard/customers" component={<Customers />} />
-          <Route path="/dashboard/customers/:id" component={<SingleCustomer />} />
-          <Route path="/dashboard/sales/analysis" component={<SalesAnalytics />} />
-          <Route path="/dashboard/sales" component={<ProductSales />} />
-          <Route path="/dashboard/orders" component={<Orders />} />
-          <Route path="/dashboard/orders/template" component={<OrderTemplate />} />
-          <Route path="/dashboard/orders/:id" component={<SingleOrder />} />
-          <Route path="/dashboard/suppliers" component={<Suppliers />} />
-          <Route path="/dashboard/transactions" component={<Transactions />} />
-          <Route path="/dashboard/brands" component={<Brands />} />
-          <Route path="/dashboard/reviews" component={<Reviews />} />
-          <Route path="/dashboard/settings" component={<Settings />} />
-          <Route path="/dashboard/reviews" component={<Reviews />} />
-          <Route path="/dashboard/inbox" component={<Inbox />} />
-   
-        </Routes>
-        
-        <Footer />
-      </Box>
-    </Box>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,30 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router ,createBrowserRouter,RouterProvider} from "react-router-dom";
-import "./styles/index.css";
-import App from "./App";
-import { Login } from "./pages";
-import { ThemeToggleProvider } from "./contexts/ThemeContext";
-import { CssBaseline } from "@mui/material";
-import Sidebar from "./components/common/Sidebar";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const router = createBrowserRouter([
-  {
-    path:"/",
-    element:<Login/>
-  },
-  {
-    path:"/dashboard",
-    element:<ThemeToggleProvider>
-              <CssBaseline />
-                  <App/>
-            </ThemeToggleProvider>
-  }
-])
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-     <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
