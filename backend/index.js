b993f3bfe3cbc58341c3012a230599e1d59243a7
Martin <ilhammartin2@gmail.com>
2024-06-01 11:02:14 +0700

    proxy

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,36 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import request from 'request';
 
-import userRoutes from "./routes/userRoutes.js"; 
-import pesananRoutes from "./routes/pesananRoutes.js"; 
-import clientroutes from "./routes/clientroutes.js"
-import reviewroutes from "./routes/reviewroutes.js"
-import dotenv from "dotenv";
-import express from "express";
-const app = express();
-import "./database/koneksi.js"; 
-import produkRoutes from "./routes/produkRoutes.js";
-import cors from "cors";
-
-import Midtrans from "./database/midtrands.js";
+import userRoutes from './routes/userRoutes.js';
+import pesananRoutes from './routes/pesananRoutes.js';
+import clientroutes from './routes/clientroutes.js';
+import reviewroutes from './routes/reviewroutes.js';
+import produkRoutes from './routes/produkRoutes.js';
+import Midtrans from './database/midtrands.js';
 
 dotenv.config();
 
-const port = 5000;
-
+const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors())
-app.use("/api/paymnet",Midtrans);
+app.use(cors());
+
+app.use('/api/payment', Midtrans);
 app.use(produkRoutes);
-app.use(userRoutes)
-app.use(pesananRoutes)
-app.use(clientroutes)
-app.use(reviewroutes)
+app.use(userRoutes);
+app.use(pesananRoutes);
+app.use(clientroutes);
+app.use(reviewroutes);
 
+// Proxy endpoint
+app.use('/api/proxy', (req, res) => {
+    const apiUrl = 'https://mauliya-bakeryserve.vercel.app' + req.url;
+    req.pipe(request(apiUrl)).pipe(res);
+});
 
-app.listen(port,()=>{
-    console.log(`server start at port no ${port}`)
-})
+app.listen(port, () => {
+    console.log(`Server started at port ${port}`);
+});
