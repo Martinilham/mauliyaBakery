150a929952a4822e4dfc4013b064fceca855b23e
Martin <ilhammartin2@gmail.com>
2024-06-02 15:55:54 +0700

    coba

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ import clientroutes from './routes/clientroutes.js';
 import reviewroutes from './routes/reviewroutes.js';
 import produkRoutes from './routes/produkRoutes.js';
 import Midtrans from './database/midtrands.js';
-import './database/koneksi.js'; // Ini mengimport koneksi database dan menjalankannya
+import './database/koneksi.js';
 
 dotenv.config();
 
@@ -25,11 +25,12 @@ app.use(pesananRoutes);
 app.use(clientroutes);
 app.use(reviewroutes);
 
+
 // Proxy endpoint
-app.use('/api/proxy', createProxyMiddleware({
-    target: 'https://maulia-bakeryserver.vercel.app',
-    changeOrigin: true,
-}));
+app.use('/api/proxy', (req, res) => {
+    const apiUrl = 'https://maulia-bakeryserver.vercel.app' + req.url;
+    req.pipe(request(apiUrl)).pipe(res);
+});
 
 app.listen(port, () => {
     console.log(`Server started at port ${port}`);
