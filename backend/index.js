50f1f474dffb0ab4156a87bd373bc20b001a7413
Martin <ilhammartin2@gmail.com>
2024-06-02 15:44:26 +0700

    fix

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ import clientroutes from './routes/clientroutes.js';
 import reviewroutes from './routes/reviewroutes.js';
 import produkRoutes from './routes/produkRoutes.js';
 import Midtrans from './database/midtrands.js';
-import './database/koneksi.js';
+
 
 dotenv.config();
 
@@ -27,7 +27,7 @@ app.use(clientroutes);
 app.use(reviewroutes);
 
 // Proxy endpoint
-app.use('api/proxy/',createProxyMiddleware({
+app.use('api/proxy',createProxyMiddleware({
     target: 'https://maulia-bakeryserver.vercel.app',
     changeOrigin: true,
 }))
